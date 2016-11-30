9adba4877a07edad59f5e34ee95e0023fae87555
JJ <gongjian_001@126.com>
2016-11-30 14:17:16 +0800

    Initial Commit

---
+++ JJSwiftStudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JJSwiftStudy
+//
+//  Created by JJ on 11/30/16.
+//  Copyright © 2016 jianjing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
