f242b2cd9e90369a3175deff4a3d117528225e09
JJ <gongjian_001@126.com>
2016-12-16 14:44:05 +0800

    update

--- Playground/SinglePattern.playground/Contents.swift
+++ Playground/SinglePattern.playground/Contents.swift
@@ -26,7 +26,7 @@ ff()
 class JJSinglePattern: NSObject {
     
     /*
-     这种写法不仅简洁，而且保证了单例的独一无二。在初始化类变量的时候，Apple 将会把这个初始化包装在一次 swift_once_block_invoke 中，以保证它的唯一性。另外，我们在这个类型中加入了一个私有的初始化方法，来覆盖默认的公开初始化方法，这让项目中的其他地方不能够通过 init 来生成自己的 MyManager 实例，也保证了类型单例的唯一性。如果你需要的是类似 defaultManager 的形式的单例 (也就是说这个类的使用者可以创建自己的实例) 的话，可以去掉这个私有的 init 方法。
+     这种写法不仅简洁，而且保证了单例的独一无二。在初始化类变量的时候，Apple 将会把这个初始化包装在一次 swift_once_block_invoke 中，以保证它的唯一性。另外，我们在这个类型中加入了一个私有的初始化方法，来覆盖默认的公开初始化方法，这让项目中的其他地方不能够通过 init 来生成自己的 JJSinglePattern 实例，也保证了类型单例的唯一性。如果你需要的是类似 sharedInstance 的形式的单例 (也就是说这个类的使用者可以创建自己的实例) 的话，可以去掉这个私有的 init 方法。
      */
     static let sharedInstance = JJSinglePattern()
     private override init() {}
@@ -34,5 +34,3 @@ class JJSinglePattern: NSObject {
 
 let singlePattern = JJSinglePattern.sharedInstance
 print(singlePattern)
-
-print("end")
